4fdc5bf9817d694bea284bb206daadc45221ab99
Tom Cobb <tom.cobb@diamond.ac.uk>
2020-02-26 16:42:39 +0000

    Added versiongit

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
+import os
+import sys
+
 from setuptools import setup
 
 
-# Setup information is stored in setup.cfg but this function call
-# is still necessary.
-setup()
+# Place the directory containing _version_git on the path
+for path, _, filenames in os.walk(os.path.dirname(os.path.abspath(__file__))):
+    if "_version_git.py" in filenames:
+        sys.path.append(path)
+        break
+
+from _version_git import __version__, get_cmdclass  # noqa
+
+setup(cmdclass=get_cmdclass(), version=__version__)
