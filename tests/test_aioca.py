9bfae49631891e03b02f05a3c7784dd81ba27a62
Tom Cobb <tom.cobb@diamond.ac.uk>
2020-04-09 14:57:00 +0100

    slcaken off timing tests for travis

--- tests/test_aioca.py
+++ tests/test_aioca.py
@@ -127,7 +127,7 @@ async def test_cainfo(ioc: subprocess.Popen) -> None:
 
 @pytest.mark.asyncio
 async def test_get_ne_pvs_no_throw(ioc: subprocess.Popen) -> None:
-    values = await caget([WAVEFORM, NE], throw=False, timeout=1.0)
+    values = await caget([WAVEFORM, NE], throw=False, timeout=2.0)
     assert [True, False] == [v.ok for v in values]
     assert pytest.approx([]) == values[0]
     ioc.communicate("exit")
