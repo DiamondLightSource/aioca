fb5887ed6bdd8a4aff526b148475791ec177bd7b
Dominic Oram <dominic.oram@diamond.ac.uk>
2024-10-04 16:16:38 +0100

    Added test that attempts to test contexts

--- tests/test_aioca.py
+++ tests/test_aioca.py
@@ -10,6 +10,7 @@ from asyncio.events import AbstractEventLoop
 from pathlib import Path
 from typing import Callable, List, Tuple, Union
 
+import threading
 import pytest
 from _pytest.unraisableexception import catch_unraisable_exception
 from epicscorelibs.ca import cadef, dbr
@@ -619,6 +620,29 @@ def test_import_in_a_different_thread(ioc: subprocess.Popen) -> None:
         ]
     )
     assert output.strip() == b"42"
+
+
+def test_read_pvs_from_different_threads(ioc: subprocess.Popen) -> None:
+    wait_for_ioc(ioc)
+    returned_values = []
+    threads = []
+
+    async def get_value():
+        returned_values.append(await caget(LONGOUT, timeout=0.5))
+        await asyncio.sleep(1)
+
+    for _ in range(2):
+        # Run event loop in a different thread
+        t = threading.Thread(
+            target=asyncio.new_event_loop().run_until_complete, args=[get_value()]
+        )
+        threads.append(t)
+        t.start()
+
+    for t in threads:
+        t.join()
+
+    assert len(returned_values) == 2
 
 
 @pytest.mark.asyncio
